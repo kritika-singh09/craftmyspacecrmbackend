c559d111fa1400f2d7595831ad57f17c34fcdeee
Kritika Singh <kritikasingh273013@gmail.com>
2026-01-21 13:51:34 +0530

    files

--- src/server.js
+++ src/server.js
@@ -31,7 +31,26 @@ const server = http.createServer(app);
 initSocket(server);
 
 // Middleware
-app.use(cors());
+// CORS Configuration
+const allowedOrigins = [
+    'http://localhost:5173',
+    'http://localhost:5000',
+    'https://craftmyspacecrmfrontend.vercel.app',
+    'https://craftmyspacecrmbackend.vercel.app'
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json());
 
